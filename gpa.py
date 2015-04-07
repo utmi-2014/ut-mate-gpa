6f005a65c31e264e158caa9ccc0073ed6d13391b
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-04-08 00:44:30 +0900

    [gpa.py] exception for 未受験 or 不可

--- gpa.py
+++ gpa.py
@@ -22,11 +22,20 @@ table = soup.find('table', {'class': 'normal'})
 trs = table.find_all('tr')
 points = {u'優上': 4.3, u'優': 4., u'良': 3., u'可': 2., u'不可': 0.}
 credits = collections.defaultdict(int)
+exception_flag = 0
 for tr in trs[1:]:
     tds = tr.find_all('td')
-    credit = float(tds[7].text)
-    grade = tds[10].text
-    credits[grade] += credit
+    try:
+        credit = float(tds[7].text)
+        grade = tds[10].text
+        credits[grade] += credit
+    except Exception:
+        if exception_flag == 1:
+            continue
+        print u'不可または未受験があります。'
+        print u'不可はGPAにおそらく反映されるので注意してください。'
+        print u'(不可は単位数がわからないので、このスクリプトは使えません)'
+        exception_flag = 1
 
 for grade, _ in sorted(points.items(), key=lambda x: x[1], reverse=True):
     print u'{}: {}'.format(grade, credits[grade])
